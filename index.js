c3911e8c1bab56857d9e8d82d9b39e3ca9b20e06
dzzaxbot <dsqzdqsdz3@gmail.com>
2025-04-28 23:00:06 +0100

    Update index.js

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.listen(port, () => {
   console.log('\x1b[36m[ SERVER ]\x1b[0m', `\x1b[32m SH : http://localhost:${port} ✅\x1b[0m`);
 });
 
-const statusMessages = [" 📌 لا تنسو", " ❤ ذكر الله"];
+const statusMessages = [" 📌 اكثرو من الاستغفار ", " ❤  ولا تنسو ذكر الله"];
 const statusTypes = ['dnd', 'idle'];
 let currentStatusIndex = 0;
 let currentTypeIndex = 0;
